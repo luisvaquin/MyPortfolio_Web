4605b35ae23c10f0dcf102f87173a57cb1879a48
luisvaquin <luisrodrigo2330@gmail.com>
2024-10-23 18:18:49 -0600

    upload route picture

--- src/pages/aboutMe.jsx
+++ src/pages/aboutMe.jsx
@@ -8,7 +8,7 @@ const AboutMe = () => {
     useEffect(() => {
         const updateBackground = () => {
             if (window.innerWidth <= 768) {
-                setBackgroundImage('../../public/imgFondLuis8.jpeg'); // Imagen para móviles
+                setBackgroundImage('/public/imgFondLuis8.jpeg'); // Imagen para móviles
             } else {
                 setBackgroundImage('https://images.unsplash.com/photo-1516101922849-2bf0be616449?q=80&w=2128&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); // Imagen para escritorio
             }
